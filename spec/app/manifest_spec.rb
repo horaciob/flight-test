c64b2923dc280ab79a2e67080378bd7af1715466
Horacio Branciforte <horaciob@gmail.com>
2019-12-10 22:28:18 +0100

    add some test

--- spec/app/manifest_spec.rb
+++ spec/app/manifest_spec.rb
@@ -1,10 +1,33 @@
 # frozen_string_literal: true
 
-describe Manifest
-describe '#add' do
-  it 'allows to add new user' do
-    m = Manifest.new
-    m.add('Horacio', %w[A1 B1])
-    expect(m.list).to eq [['Horacio', %w[A1 B1]]]
+describe Manifest do
+  describe '#add' do
+    it 'allows to add new user' do
+      m = Manifest.new
+      m.add('Horacio', %w[A1 B1])
+      expect(m.list).to eq [['Horacio', %w[A1 B1]]]
+    end
+  end
+
+  describe '#show resume' do
+    it 'show users descriptions' do
+      m = Manifest.new
+      m.add('Horacio', %w[A1 B1])
+      expect { m.show_resume }.to output(/\Horacio: 2 people/).to_stdout
+    end
+
+    it 'show singular when is just 1 book' do
+      m = Manifest.new
+      m.add('Horacio', %w[A1])
+      expect { m.show_resume }.to output(/\Horacio: 1 person/).to_stdout
+    end
+  end
+
+  describe '#show details' do
+    it 'show users descriptions' do
+      m = Manifest.new
+      m.add('Horacio', %w[A1 B1])
+      expect { m.show_details }.to output("* Horacio seats: A1, B1\n").to_stdout
+    end
   end
 end
--- spec/app/short_airplane_spec.rb
+++ spec/app/short_airplane_spec.rb
@@ -12,4 +12,24 @@ describe ShortAirplane do
       expect(sort_airplane.row_arrangement).to eq 'ABC_DEF'
     end
   end
+
+  describe '#reserve' do
+    context 'creates a chain of strategies that will run in order' do
+      it 'Row handler should receive process_request message' do
+        handler = instance_double(RowHandler)
+        allow(RowHandler).to receive(:new).and_return(handler)
+        expect(handler).to receive(:process_request)
+          .with(instance_of(ShortAirplane), 2)
+
+        described_class.new.reserve(2)
+      end
+
+      it "Random handler has to be the Row handler's successor" do
+        expect(RowHandler).to receive(:new)
+          .with(instance_of(RandomHandler)).and_call_original
+
+        described_class.new.reserve(2)
+      end
+    end
+  end
 end
